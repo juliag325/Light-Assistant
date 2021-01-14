9e0c1c031bd7b84bb2f74429a82a1a531426aad8
Julia <juliag325@gmail.com>
2021-01-13 19:17:16 -0500

    Increment light count

--- countLights.py
+++ countLights.py
@@ -20,7 +20,32 @@ def countLights():
 	lightList[] = ['directionalLight', 'pointLight', 'spotLight', 'ambientLight', 'areaLight', 'volumeLight']
 	print "this works"
 
-	objects = cmds.ls()
-	
+	objects = cmds.ls()	
+
+	# Get all light objects in the scene 
+	lights = cmds.ls(lights = True)
+
 	# For all the objects in the directory, we are going to list the number of each light
-	for i in objects: 
+	for light in  lights: 
+		
+		lightType = cmds.nodeType(light)
+
+		if lightType == lightList[0]:
+			lightCount[0] += 1
+
+		elif lightType == lightList[1]:
+			lightCount[1] += 1
+
+		elif lightType == lightList[2]:
+			lightCount[2] += 1
+
+		elif lightType == lightList[3]:
+			lightCount[3] += 1
+
+		elif lightType == lightList[4]:
+			lightCount[4] += 1
+
+		elif lightType == lightList[5]:
+			lightCount[5] += 1
+
+	print lightCount
